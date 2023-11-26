386e143dec53fe4813927bc2bac4e72026599816
Ye-jin Jeon <jyj@Ye-jinui-MacBook-Pro.local>
2023-11-26 22:09:46 +0900

    practice2

--- src/components/A.jsx
+++ src/components/A.jsx
@@ -2,22 +2,26 @@ import tossvideo from "./tossvideo"; // eslint-disable-line no-unused-vars
 
 const A = () => {
   return (
-    <>
-      <div className="z-20 bg-black flex flex-col text-center text-6xl font-bold">
-        <p class="text-gray-300">토스의 새로운 채용 전형</p>
-        <p class="text-white mt-6">프러덕트 디자이너 챌린지</p>
-        <p class="text-gray-500 text-2xl mt-8">
+    <div className="relative min-h-screen bg-black flex flex-col justify-center items-center text-center text-6xl font-bold">
+      <div className="absolute flex flex-col justify-center pb-60">
+        <p className="text-gray-300">토스의 새로운 채용 전형</p>
+        <p className="text-white mt-6">프러덕트 디자이너 챌린지</p>
+        <p className="text-gray-500 text-2xl mt-8">
           본 전형의 접수가 마감 되었습니다. 이후 지원은 토스 수시 채용을
           이용해주시기 바랍니다.
         </p>
-        <div class="flex items-center justify-center h-auto w-screen mb-12 overflow-hidden">
-          <div class="absolute z-10 p-5 text-2xl text-black bg-gray-300 rounded-xl">
-            채용홈페이지 바로가기!
-          </div>
-          <tossvideovideo />
-        </div>
       </div>
-    </>
+      <div className="absolute bottom-40 z-10 p-5 text-2xl text-black bg-gray-300 rounded-xl">
+        채용홈페이지 바로가기!
+      </div>
+      <video
+        className="absolute bottom-0"
+        src="https://static.toss.im/assets/homepage/next-pd-2022/looping_film.mp4"
+        autoPlay
+        muted
+        loop
+      ></video>
+    </div>
   );
 };
 
