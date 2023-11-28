9384cd67e7253ff405615aa64051b41cac5d0366
Ye-jin Jeon <jyj@Ye-jinui-MacBook-Pro.local>
2023-11-28 11:58:38 +0900

    “practice git push -u origin main git commit -m “practice03

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import A from "./components/A";
-import B from "./components/B";
-import C from "./components/C";
-import D from "./components/D";
+//import B from "./components/B";
+//import C from "./components/C";
+//import D from "./components/D";
 import Header from "./components/Header";
 import Headercopy from "./components/Headercopy"; // eslint-disable-line no-unused-vars
 
@@ -10,9 +10,6 @@ const App = () => {
     <>
       <Header />
       <A />
-      <B />
-      <C />
-      <D />
     </>
   );
 };
